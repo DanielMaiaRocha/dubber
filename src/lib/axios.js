722c990a806850d8ad16c87f385a16690d85e1ae
Daniel <cronobr5@gmail.com>
2025-02-17 17:59:41 -0300

    axios Lib conn

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.mode === "development" ? "http://api-dubber.onrender.com/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" 
+    ? "http://localhost:8800/api"  // Se rodando localmente, usa o localhost
+    : "https://api-dubber.onrender.com/api", // Se rodando no Vercel, usa a API no Render
   withCredentials: true,
 });
-
 export default axiosInstance;
